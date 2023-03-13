a960ab919c16c2cbd084629a6f8fe4a0e38c4f25
FaithAyo <faithojetayo@gmail.com>
2023-03-13 06:42:52 -0700

    finally

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <unistd.h>
+/**
+ * main - this is to print a quote
+ * Return: 1
+ */
+int main(void)
+
+{
+char *messag = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, messag, 60);
+	return (1);
+}
