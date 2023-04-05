2b0d70fc72bca59d43755a25cc8d7e9b9a2e21b8
FaithAyo <faithojetayo@gmail.com>
2023-04-05 05:32:50 -0700

    print the list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * size_t - the fxn that prints all the element
+ * @h: head that points to the first element
+ * Return: the number of the element of listint_t list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+	       h = h->next;
+		count++;
+	}
+	return(count);
+}
+
