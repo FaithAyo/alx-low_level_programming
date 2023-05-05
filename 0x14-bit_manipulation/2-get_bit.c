64d669b03493eb63d0fbe547076952638715fbee
FaithAyo <faithojetayo@gmail.com>
2023-05-04 19:54:38 -0700

    bit based on index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,13 +5,12 @@
  * @index: unsigned int bit at a given index
  * Return: the value of the bit or -1 if error occured
  */
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index)
 {
-	int a;
 
-	if (index > 20)
+	if (index > 99)
 		return (-1);
 
-	for (a = 0; n; a++)
-	{
-		if (a 
+	return ((n >> index) & 1);
+
+}
