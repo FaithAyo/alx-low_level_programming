19fb9767133b8d15e332e6eee982afb8422477e6
FaithAyo <faithojetayo@gmail.com>
2023-05-04 19:16:41 -0700

    still in progress

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * get_bit - returns the value of a bit
+ * @n: unsigned long int
+ * @index: unsigned int bit at a given index
+ * Return: the value of the bit or -1 if error occured
+ */
+int get_bit(unsigned long int n, unsigned int index);
+{
+	index
