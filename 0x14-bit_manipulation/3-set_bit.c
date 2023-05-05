b698f0ad8f03dafcebe6417ed2acaa35ea3200cb
FaithAyo <faithojetayo@gmail.com>
2023-05-04 21:55:25 -0700

    in progress

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * set_bit - sets the value of bit to 1
+ * @n: unsigned long int
+ * @index: the given index
+ * Return: 1 if it works and -1 if an error occured
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
