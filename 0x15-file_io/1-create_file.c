1e819bd1f003436657ab405f2765ed2494cd0df8
FaithAyo <faithojetayo@gmail.com>
2023-05-08 21:41:00 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		text_content = " ";
+		text_content = "";
 
 	length_no = strlen(text_content);
 
